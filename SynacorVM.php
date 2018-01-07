eda3d91001bdef97ae1a25fd6c0f9ec223175e35
Anthony Hersey <anthony@stormsweeper.net>
2018-01-07 11:26:52 -0500

    here's hoping for good stuff

--- SynacorVM.php
+++ SynacorVM.php
@@ -26,7 +26,6 @@ class SynacorVM
     }
 
     function readMemory(int $loc) {
-        //echo "read mem {$loc}\n";
         $word = substr($this->memory, $loc * 2, 2);
         return unpack('v', $word)[1];
     }
@@ -52,39 +51,73 @@ class SynacorVM
         if ($cmd > 21) {
             throw new RuntimeException("Not a valid cmd: {$cmd}");
         }
-        $this->{self::OPCODES[$cmd]}();
+        //echo self::OPCODES[$cmd] . "\n";
+        $this->{'op_' . self::OPCODES[$cmd]}();
     }
 
     function nextArg() {
         return $this->coerceValue($this->readMemory($this->cursor++));
     }
 
+    function nextRegister() {
+        $reg = $this->readMemory($this->cursor++);
+        if ($reg < self::MAX_INT || $reg >= self::MAX_INT + 8) {
+            throw new RuntimeException("Value is not a register: {$reg}");
+        }
+        return $reg - self::MAX_INT;
+    }
+
     function run() {
         while (true) {
+            //echo "\nCURSOR {$this->cursor}\n";
             $this->nextInstruction();
         }
     }
 
-    function halt() {
+    function op_halt() {
         exit();
     }
 
-    function set() {
-        $a = $this->nextArg();
+    function op_set() {
+        $a = $this->nextRegister();
         $b = $this->nextArg();
         $this->setRegister($a, $b);
     }    
 
-    function push() {
+    function op_push() {
         $a = $this->nextArg();
         array_push($this->stack, $a);
     }
 
-    function jmp() {
-        $this->cursor = $this->nextArg();
+    function op_pop() {
+        $a = $this->nextRegister();
+        if (!$this->stack) {
+            throw new RuntimeException("Can't pop empty stack");
+        }
+        $this->setRegister($a, array_pop($this->stack));
     }
 
-    function jt() {
+    function op_eq() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $c = $this->nextArg();
+        $val = $b === $c ? 1 : 0;
+        $this->setRegister($a, $val);
+    }
+
+    function op_gt() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $c = $this->nextArg();
+        $val = $b > $c ? 1 : 0;
+        $this->setRegister($a, $val);
+    }
+
+    function op_jmp() {
+        $this->cursor = $this->nextArg();;
+    }
+
+    function op_jt() {
         $a = $this->nextArg();
         $b = $this->nextArg();
         if ($a > 0) {
@@ -92,12 +125,100 @@ class SynacorVM
         }
     }
 
-    function out() {
+    function op_jf() {
+        $a = $this->nextArg();
+        $b = $this->nextArg();
+        if ($a === 0) {
+            $this->cursor = $b;
+        }
+    }
+
+    function op_add() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $c = $this->nextArg();
+        $val = ($b + $c) % self::MAX_INT;
+        $this->setRegister($a, $val);
+    }
+
+    function op_mult() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $c = $this->nextArg();
+        $val = ($b * $c) % self::MAX_INT;
+        $this->setRegister($a, $val);
+    }
+
+    function op_mod() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $c = $this->nextArg();
+        $val = $b % $c;
+        $this->setRegister($a, $val);
+    }
+
+    function op_and() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $c = $this->nextArg();
+        $val = ($b & $c + self::MAX_INT) % self::MAX_INT;
+        $this->setRegister($a, $val);
+    }
+
+    function op_or() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $c = $this->nextArg();
+        $val = $b | $c;
+        $this->setRegister($a, $val);
+    }
+
+    function op_not() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $val = (~ $b + self::MAX_INT) % self::MAX_INT;
+        $this->setRegister($a, $val);
+    }
+
+    function op_rmem() {
+        $a = $this->nextRegister();
+        $b = $this->nextArg();
+        $val = $this->readMemory($b);
+        $this->setRegister($a, $val);
+    }
+
+    function op_wmem() {
+        $a = $this->nextArg();
+        $b = $this->nextArg();
+        $this->writeMemory($a, $b);
+    }
+
+    function op_call() {
+        $a = $this->nextArg();
+        array_push($this->stack, $this->cursor);
+        $this->cursor = $a;
+    }
+
+    function op_ret() {
+        if (!$this->stack) {
+            throw new RuntimeException("Can't ret from empty stack");
+        }
+        
+        $this->cursor = array_pop($this->stack);
+    }
+
+    function op_out() {
         $ascii = $this->nextArg();
         echo chr($ascii);
     }
 
-    function noop() {
+    function op_in() {
+        $a = $this->nextRegister();
+        $input = ord(fgetc(STDIN));
+        $this->setRegister($a, $input);
+    }
+
+    function op_noop() {
         
     }
 
